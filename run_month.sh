d37d9147faf9a1b5e6efe7d36b2b688fc84cccbf
unknown <mislav.sagovac@contentio.biz>
2023-12-20 09:09:36 +0100

    increase mem

--- run_month.sh
+++ run_month.sh
@@ -1,12 +1,11 @@
+#!/bin/bash
 
 #PBS -N PEAD
 #PBS -l ncpus=4
-#PBS -l mem=10GB
-#PBS -J 10000-10000
+#PBS -l mem=16GB
+#PBS -J 1-8784
 #PBS -o experimentsmonth/logs
 #PBS -j oe
 
 cd ${PBS_O_WORKDIR}
 apptainer run image.sif run_job.R 0
-
-# 1-18720
