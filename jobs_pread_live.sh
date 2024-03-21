065ce57a340982d543ac59fdcc8b7f678404529e
MislavSag <mislav.sagovac@contentio.biz>
2024-03-21 10:50:16 +0100

    PEAD

--- jobs_pread_live.sh
+++ jobs_pread_live.sh
@@ -2,10 +2,12 @@
 
 #PBS -N PEAD
 #PBS -l ncpus=4
-#PBS -l mem=8GB
-#PBS -J 1-11
+#PBS -l mem=16GB
+#PBS -J 1-6
 #PBS -o experiments_pread_live/logs
 #PBS -j oe
 
 cd ${PBS_O_WORKDIR}
 apptainer run image.sif run_job.R 0 experiments_pread_live
+
+# 11 - 5
