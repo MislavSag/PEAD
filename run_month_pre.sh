782ea792e041f828c0875fd5c9157fb9d405c2d5
MislavSag <mislav.sagovac@contentio.biz>
2024-04-10 14:53:46 +0200

    PREAD

--- run_month_pre.sh
+++ run_month_pre.sh
@@ -3,10 +3,9 @@
 #PBS -N PEAD
 #PBS -l ncpus=4
 #PBS -l mem=32GB
-#PBS -J 1-11
-#PBS -o experiments_pread_live/logs
+#PBS -J 1-605
+#PBS -o experiments_pre/logs
 #PBS -j oe
-
 
 cd ${PBS_O_WORKDIR}
 apptainer run image.sif run_job.R 0 experiments_pre
